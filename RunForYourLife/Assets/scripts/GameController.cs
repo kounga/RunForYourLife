740e6aab987e3bd2a076e104ab647521e5dcabde
Kevin Leblanc <kevin.leblanc.1@etsmtl.net>
2015-10-19 15:26:57 -0400

    Game and Player controllers

---
+++ RunForYourLife/Assets/scripts/GameController.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+    public float gameSpeed = 1f;
+    float gameTime = 0f;
+    public bool gameRunning = false;
+    public float difficulty = 1f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        if (gameRunning)
+        {
+            //Set game speed
+            gameTime += Time.deltaTime;
+            gameSpeed = Mathf.Sqrt(gameTime * 0.1f);
+        }
+	}
+
+    //Probably remove this
+    void Update()
+    {
+        if (Input.GetButtonDown("Start_Game"))
+        {
+            StartGame();
+        }
+    }
+
+    void StartGame()
+    {
+        gameRunning = true;
+    }
+
+
+}
---
+++ RunForYourLife/Assets/scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    public bool jumpingPressed = false;
+    public bool jumping = false;
+    float jumpTimer = 0f;
+
+	// Update is called once per frame
+	void Update () {
+	    if (Input.GetButtonDown("Jump"))
+            jumpingPressed = true;
+        if (Input.GetButtonUp("Jump"))
+        {
+            jumpingPressed = false;
+            jumping = true;
+        }
+        if (jumping)
+        {
+            gameObject.GetComponent<"">
+        }
+	}
+
+    void FixedUpdate()
+    {
+        if (jumpingPressed)
+            jumpTimer += Time.fixedDeltaTime;
+    }
+}
